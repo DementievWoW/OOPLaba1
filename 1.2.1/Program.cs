f1cd2aa0ff197d75b7557d8a8f8ed0fe56740eee
DementievWoW <dementievwow@mail.ru>
2023-06-06 11:00:58 +0300

    1 и 2 уровень, первые 3 задания

---
+++ 1.2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+double member = 1;
+double s = 0;
+int n = 0;
+var rnd= new Random();
+double x= rnd.NextDouble()*1.9-1;
+while (member>=0.0001)
+{
+    n++;
+    member= (Math.Cos(n * x)) / (n * n);
+    s +=member ;
+    Console.WriteLine(member);
+}
+Console.WriteLine(s);
---
+++ 1.2.3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите p");
+int i, sum, p, count;
+p = Convert.ToInt16(Console.ReadLine());
+sum = 0;
+count = 0;
+for (i = 1; i <= p; i++)
+{
+    count++;
+    sum += i;
+    if ( sum>p)
+    {
+        break;
+    }
+    
+    Console.WriteLine(sum);
+}
+
+Console.WriteLine(count);
